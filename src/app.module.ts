b881ea32df6609b94eabd296e1aa40f06d70fb6b
Ikromxon <ikromkhanshukrullayev03022005@gmail.com>
2025-05-28 14:53:21 +0500

    okee

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,18 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthorModule } from './author/author.module';
 import { BookModule } from './book/book.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule, AuthorModule, BookModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env', // default holatda ham ishlaydi
+    }),
+    PrismaModule,
+    AuthorModule,
+    BookModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
